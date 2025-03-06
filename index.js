134fe8f093be07c9e57f9f804ad3e7d0d6fcfb5a
kelvin Githinji <githinjikelvin74@gmail.com>
2025-03-06 16:32:19 +0300

    Adds test

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(somevalue){
   // Write your code here!
+  if(somevalue <= 400){
+    return 'This one is on me!'
+  }else if(somevalue > 400 && somevalue <= 2000){
+    return 'That will be twenty bucks.'
+  }else if(somevalue > 2000 && somevalue <=2500){
+    return 'I will gladly take your thirty bucks.'
+  }else{
+    return 'No can do.'
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'?'Ok, sounds good.' : 'No go.'
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.' 
+    default:
+      return 'Bye.'
+  }
+} 
